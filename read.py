57a709144ca7a6da9ec5293b232b525033e01598
39Eugene <prabinrau0@gmail.com>
2024-04-22 09:56:40 +0545

    display list function to check the availablitiy of land

--- read.py
+++ read.py
@@ -23,5 +23,24 @@ def read_f1():
 
 new_lists = read_f1()
 
-for new_dict in new_lists:
-    print(new_dict)
+'''for new_dict in new_lists:
+    print(new_dict)'''
+
+def display_lists(lists):
+    print("Availabe lands:" )
+    for new_dict in lists:
+        if new_dict["status"] == "Available":
+            print(new_dict["kitta_no"] + ":" + new_dict["city"] + "- " + new_dict["land_direction"] + ", Area:" + new_dict["area"] + "anna, Price:" + new_dict["price"])
+
+
+    print("\n Not Availabe lands:" )
+    for new_dict in lists:
+        if new_dict["status"] == "Not Available":
+            print(new_dict["kitta_no"] + ":" + new_dict["city"] + "- " + new_dict["land_direction"] + ", Area:" + new_dict["area"] + "anna, Price:" + new_dict["price"])
+
+
+# Call read_f1() to read land details
+new_lists = read_f1()
+
+# Call display_lists() to display availability
+display_lists(new_lists)
