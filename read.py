5991caf6890c7dad3e6fd2b9c485cc2a29521738
39Eugene <prabinrau0@gmail.com>
2024-04-22 09:35:43 +0545

    to print the dictionary

--- read.py
+++ read.py
@@ -23,14 +23,5 @@ def read_f1():
 
 new_lists = read_f1()
 
-'''# Print the data in the console
-for item in new_lists:
-    print("Kitta Number:", item["kitta_no"])
-    print("City:", item["city"])
-    print("Land Direction:", item["land_direction"])
-    print("Area:", item["area"])
-    print("Price:", item["price"])
-    print("Status:", item["status"])
-    print()  # Empty line for better readability between entries '''
-
-#to check
+for new_dict in new_lists:
+    print(new_dict)
